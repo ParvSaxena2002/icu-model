f6c227dd12f655f7f5d8a7218678efd7c7b60388
ParvSaxena2002 <126588027+ParvSaxena2002@users.noreply.github.com>
2025-11-03 15:34:51 +0530

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,5 +1,5 @@
 ﻿import joblib, pandas as pd
-df = pd.read_csv("data/processed/vitals_combined_labeled.csv")
+df = pd.read_csv("deployment_package/sample_data/vitals_combined_labeled.csv")
 model = joblib.load("models/vitals_model.joblib")
 
 sample = df.select_dtypes(include="number").iloc[0:1].drop(columns=["label"], errors="ignore")
